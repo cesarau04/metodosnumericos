9b1625b38619db801c22adbcbee6e286bfb45776
César Augusto <cesarau04@gmail.com>
2018-08-07 09:18:02 -0500

    Update find_index.py

--- find_index.py
+++ find_index.py
@@ -1,3 +1,17 @@
+"""
+Create a function that takes a list and a string as arguments and return the index of the string.
+Examples
+
+['hi', 'edabit', 'fgh', 'abc'], 'fgh' ➞ 2
+
+['Red', 'blue', 'Blue', 'Green'], 'blue' ➞ 1
+
+['a', 'g', 'y', 'd'], 'd' ➞ 3
+
+['Pineapple', 'Orange', 'Grape', 'Apple'], 'Pineapple' ➞ 0
+"""
+
+
 def find_index(lst, str):
 	for word in lst:
 		if word==str:
